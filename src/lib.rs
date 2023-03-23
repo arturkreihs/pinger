0fa6fbd6569106486c40990a57c822d6b8973553
Artur Kreihs <akreihs@ascs.pl>
2023-03-23 16:11:32 +0000

    payload simplification

--- src/lib.rs
+++ src/lib.rs
@@ -10,20 +10,16 @@ use std::thread::sleep;
 
 pub struct Pinger {
     payload: Vec<u8>,
-    chcksum: u8,
     sock: IcmpSocket4,
 }
 
 impl Pinger {
     pub fn new() -> Result<Self> {
-        let payload = vec![0u8,1,2,3,4,5,6,7];
-        let chcksum: u8 = payload.iter().sum();
         let mut sock = IcmpSocket4::new()?;
         sock.bind("0.0.0.0".parse::<Ipv4Addr>().unwrap())?;
         sock.set_timeout(Some(Duration::from_secs(1)));
         Ok(Self {
-            payload,
-            chcksum,
+            payload: vec![0u8],
             sock,
         })
     }
@@ -34,8 +30,8 @@ impl Pinger {
         self.sock.send_to(addr, pkt)?;
         loop {
             let (resp, _) = self.sock.rcv_from().or(Err(Error::from(ErrorKind::TimedOut)))?;
-            if let Icmpv4Message::EchoReply { identifier, sequence: _, payload } = resp.message {
-                if identifier != 42 || payload.iter().sum::<u8>() != self.chcksum {
+            if let Icmpv4Message::EchoReply { identifier: id, sequence: _, payload: pd } = resp.message {
+                if id != 42 || pd.get(0) != Some(&0) {
                     return Err(Error::from(ErrorKind::InvalidData));
                 }
                 return Ok(());
