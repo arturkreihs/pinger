6b3ab289c39c6038bbe0a0c08d192b89acd11c58
Artur Kreihs <akreihs@ascs.pl>
2023-05-26 11:57:29 +0200

    mapping errors using thiserror

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,27 @@ use std::{
     time::Duration,
 };
 
-use icmp_socket::packet::WithEchoRequest;
+use icmp_socket::packet::{
+    WithEchoRequest,
+};
 use icmp_socket::*;
-use std::io::{Result,Error,ErrorKind};
 use std::thread::sleep;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum PingerError {
+    #[error("invalid response received")]
+    InvalidResponse,
+
+    #[error("invalid address")]
+    InvalidAddress(#[from] std::net::AddrParseError),
+
+    #[error("I/O")]
+    IO(#[from] std::io::Error),
+
+    #[error("creating ICMP packet")]
+    PktCreation,
+}
 
 pub struct Pinger {
     payload: Vec<u8>,
@@ -14,7 +31,7 @@ pub struct Pinger {
 }
 
 impl Pinger {
-    pub fn new() -> Result<Self> {
+    pub fn new() -> Result<Self, PingerError> {
         let mut sock = IcmpSocket4::new()?;
         sock.bind("0.0.0.0".parse::<Ipv4Addr>().unwrap())?;
         sock.set_timeout(Some(Duration::from_secs(1)));
@@ -24,15 +41,15 @@ impl Pinger {
         })
     }
 
-    pub fn ping(&mut self, addr: &str) -> Result<()> {
-        let addr = addr.parse::<Ipv4Addr>().or(Err(Error::from(ErrorKind::InvalidInput)))?;
-        let pkt = Icmpv4Packet::with_echo_request(42, 0, self.payload.clone())?;
+    pub fn ping(&mut self, addr: &str) -> Result<(), PingerError> {
+        let addr = addr.parse::<Ipv4Addr>()?;
+        let pkt = Icmpv4Packet::with_echo_request(42, 0, self.payload.clone()).map_err(|_| PingerError::PktCreation)?;
         self.sock.send_to(addr, pkt)?;
         loop {
-            let (resp, _) = self.sock.rcv_from().or(Err(Error::from(ErrorKind::TimedOut)))?;
+            let (resp, _) = self.sock.rcv_from()?;
             if let Icmpv4Message::EchoReply { identifier: id, sequence: _, payload: pd } = resp.message {
                 if id != 42 || pd.get(0) != Some(&0) {
-                    return Err(Error::from(ErrorKind::InvalidData));
+                    return Err(PingerError::InvalidResponse);
                 }
                 return Ok(());
             }
@@ -40,4 +57,3 @@ impl Pinger {
         }
     }
 }
-
